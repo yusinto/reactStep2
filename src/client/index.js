c593008a951e2311daa4a721d98fe845b62a4ede
Yusinto Ngadiman <yngadiman@bauer-media.com.au>
2016-03-30 16:09:50 +1100

    Added comments as part of step 2 blog content

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,8 @@
+//We need the render method of the react-dom package to mount our component onto an html element
 import React from 'react';
 import {render} from 'react-dom';
 import App from '../common/component/appComponent';
 
+// This is the entry point into our react app on the client side. Again we use jsx to create our ReactElement to be
+// mounted onto a div called reactDiv on the html template.
 render(<App />, document.getElementById('reactDiv'));
